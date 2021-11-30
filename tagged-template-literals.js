077c667f4d042a80bc827437de0919811cf8778c
John Riviello <github@riviello.net>
2021-11-30 16:00:12 -0500

    First script, and hopeful Eagles record

--- tagged-template-literals.js
+++ tagged-template-literals.js
@@ -1,10 +1,10 @@
-const record = { wins: 3, losses: 7 };
+const record = { wins: 6, losses: 7 };
 // first argument is an array of strings, other arguments are the expressions
 function myTag(strings, winExpression, lossExpression) {
   const str0 = strings[0]; // "The Eagles have "
   const str1 = strings[1]; // " wins and "
   const str2 = strings[2]; // " losses."
-  const remaining = 16 - winExpression - lossExpression;
+  const remaining = 17 - winExpression - lossExpression;
   const winning = winExpression > lossExpression;
   const strategy = winning ? 'win some more games' : 'figure out how to win';
   return `${str0}${winExpression}${str1}${lossExpression}${str2}
@@ -12,5 +12,5 @@ function myTag(strings, winExpression, lossExpression) {
 }
 let output = myTag`The Eagles have ${record.wins} wins and ${record.losses} losses.`;
 
-console.log(output); // The Eagles have 3 wins and 7 losses.
-                     //   With 6 games to go, they better win some more games!
+console.log(output); // The Eagles have 6 wins and 7 losses.
+                     //   With 4 games to go, they better win some more games!
